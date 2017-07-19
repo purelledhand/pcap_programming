52fc7da44d3db26c4194c8f69b28e771b3bcf4df
purelledhand <wergreat10@naver.com>
2017-07-19 09:51:08 -0700

    I can recrieve packetsgit add packet_capture_hard.c

--- packet_capture_hard.c
+++ packet_capture_hard.c
@@ -2,12 +2,34 @@
 #include<pcap.h>
 
 int main(){
-
+    pcap_t *handle;
     char* dev;
     char errbuf[PCAP_ERRBUF_SIZE];
+    const u_char *packet;
+    struct pcap_pkthdr *header;
 
     dev = pcap_lookupdev(errbuf);
-    printf("%s\n",dev);
+    printf("interface name : %s\n",dev);
+
+    handle = pcap_open_live(dev, 1000, 0, -1, errbuf);
+    if(handle == NULL) {
+     return 0;
+    }
+    
+    int packet_status = 0;
+    while(1){
+   
+    packet_status = pcap_next_ex(handle, &header, &packet);
+    if(packet_status == 0)
+      continue;
+    else if(packet_status == -1 || packet_status == -2)
+      return 0;
+    
+    printf("%.02x\n",packet[12]);
+
+
+    }
 
     return 0;
 }
+
